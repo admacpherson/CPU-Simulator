825ab076e4f869c9dd6214c9bf3c1f64d89e2388
admacpherson <102562791+admacpherson@users.noreply.github.com>
2024-11-09 11:32:16 -0800

    Read data from file(s)

--- main.py
+++ main.py
@@ -16,6 +16,30 @@ JMP
 
 '''
 
+# Reference input files
+DATA_INPUT_FILE = "Inputs/data_input.txt"
+INSTRUCTION_INPUT_FILE = "Inputs/instruction_input.txt"
+
+
+# Read data from file and strip whitespace
+def read_strip_data(filepath):
+    # Open file in read mode
+    file = open(filepath, 'r')
+    # Read lines from file
+    data = file.readlines()
+    # Strip whitespace using anonymous lambda function
+    data = list(
+        map(
+            lambda s: s.strip(), data
+        )
+    )
+    return data
+
+
+data_input = read_strip_data(DATA_INPUT_FILE)
+instructions_input = read_strip_data(INSTRUCTION_INPUT_FILE)
+
+# Run CPU Simulator
 print("------------------------")
 print("      CPU Simulator     ")
 print("------------------------")
@@ -31,5 +55,3 @@ cpu.parse_instructions("CACHE, 2")
 print("CPU Counter:" + str(cpu.get_cpu_counter_value()))
 cpu.parse_instructions("JMP, 1")
 print("CPU Counter: " + str(cpu.get_cpu_counter_value()))
-
-
